323bc2b900b3998dfe261d53129ec50a37589174
Souvik Pramanick <souvikpramanick2004@gmail.com>
2023-02-25 14:07:27 +0530

    Problem 1 Solved!!

---
+++ Cilk/01/general map function for a 1D array.py
@@ -0,0 +1,16 @@
+import numpy
+
+def square_array(arr):
+    return numpy.square(arr)
+
+def cube_array(arr):
+    return numpy.power(arr, 3)
+
+def square_2d_array(arr):
+    return numpy.square(arr)
+
+def cube_2d_array(arr):
+    return numpy.power(arr, 3)
+
+def map_array(func, arr):
+    return numpy.array(list(map(func, arr)))
---
+++ Cilk/01/square or cube all the elements in a given 1D or 2D array.py
@@ -0,0 +1,13 @@
+import numpy
+
+def square_array(arr):
+    return numpy.square(arr)
+
+def cube_array(arr):
+    return numpy.power(arr, 3)
+
+def square_2d_array(arr):
+    return numpy.square(arr)
+
+def cube_2d_array(arr):
+    return numpy.power(arr, 3)
